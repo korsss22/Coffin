8cbd16e38640c9895703573eaa1d018e99f1f611
byeongjun2 <byeongjun1326@gmail.com>
2025-03-07 15:23:08 +0900

    SteamManager

---
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+using Mirror.FizzySteam;
+using Edgegap;
+using Steamworks;
+
+public class LobbyManager : MonoBehaviour
+{
+    private CSteamID cSteamID = SteamUser.GetSteamID();
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameObject != null) {
+            DontDestroyOnLoad(gameObject);
+        }
+
+        Debug.Log(SteamFriends.GetFriendPersonaName(cSteamID));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
