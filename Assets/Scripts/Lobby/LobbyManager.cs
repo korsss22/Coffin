6d444f5affb7c8c68b9dd96c01f08e9b441ce287
ogeonu21 <167061004+ogeonu21@users.noreply.github.com>
2025-03-07 16:55:08 +0900

    joint

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -2,14 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
-using Mirror.FizzySteam;
+//using Mirror.FizzySteam;
 using Edgegap;
-using Steamworks;
+//using Steamworks;
 
 public class LobbyManager : MonoBehaviour
 {
+    /*
     private CSteamID cSteamID = SteamUser.GetSteamID();
-    // Start is called before the first frame update
+     Start is called before the first frame update
     void Start()
     {
         if (gameObject != null) {
@@ -19,9 +20,11 @@ public class LobbyManager : MonoBehaviour
         Debug.Log(SteamFriends.GetFriendPersonaName(cSteamID));
     }
 
-    // Update is called once per frame
+     Update is called once per frame
     void Update()
     {
         
     }
+    */
 }
+
---
+++ Assets/Scripts/Object/PointPosition.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointPosition : MonoBehaviour
+{
+    public GameObject[] playerList;
+    // Start is called before the first frame update
+    void Start()
+    {
+ 
+    }
+
+    //플레이어가 입장할때마다 플레이어 리스트를 업데이트 할 메소드 추가 필요
+
+
+    public Vector3 getAverage()
+    {
+        playerList = GameObject.FindGameObjectsWithTag("Player");
+        int listLength = playerList.Length;
+        Vector3 avVector = new Vector3(0, 0, 0);
+
+        for (int i = 0; i < listLength; i++)
+        {
+            avVector += playerList[i].transform.position;
+        }
+
+        if (listLength == 0)
+        {
+            return new Vector3(0f, 1.8f, 0f);
+        }
+
+        return avVector / listLength;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.transform.position = getAverage() + new Vector3(0f, 1.8f, 0f);
+
+    }
+}
